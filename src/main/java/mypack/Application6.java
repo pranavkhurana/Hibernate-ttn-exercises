2dc2706bccffde663b22ab12ff7f23c97cc9c73b
pranavkhurana <pranavkhurana96@gmail.com>
2017-07-04 22:29:35 +0530

    application 6

--- src/main/java/mypack/Application6.java
+++ src/main/java/mypack/Application6.java
@@ -1,17 +1,16 @@
 package mypack;
 
-import entity.Author5;
+import entity.Author6;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Application6 {
     public static void main(String[] args) {
-        //only hibernate5.cfg.xml is changed from hibernate4.cfg.xml
-        SessionFactory sessionFactory=new Configuration().configure("hibernate5.cfg.xml").buildSessionFactory();
+        SessionFactory sessionFactory=new Configuration().configure("hibernate6.cfg.xml").buildSessionFactory();
         Session session=sessionFactory.openSession();
         session.beginTransaction();
-        session.save(new Author5("John","Doe",21));
+        session.save(new Author6("John","Doe",21));
         session.getTransaction().commit();
         sessionFactory.close();
     }
