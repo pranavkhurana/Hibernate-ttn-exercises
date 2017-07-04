f38eeccb3229764fb10ee0ee2cd45fcb6eae9bc5
pranavkhurana <pranavkhurana96@gmail.com>
2017-07-04 22:22:08 +0530

    application 5

---
+++ src/main/java/mypack/Application5.java
@@ -0,0 +1,21 @@
+package mypack;
+
+import entity.Author4;
+import entity.Author5;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.GregorianCalendar;
+
+public class Application5 {
+    public static void main(String[] args) {
+        //only hibernate5.cfg.xml is changed from hibernate4.cfg.xml
+        SessionFactory sessionFactory=new Configuration().configure("hibernate5.cfg.xml").buildSessionFactory();
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Author5("John","Doe",21));
+        session.getTransaction().commit();
+        sessionFactory.close();
+    }
+}
