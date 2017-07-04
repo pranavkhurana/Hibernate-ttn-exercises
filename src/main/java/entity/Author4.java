46f79e40f867a348346aacd80d4817137b37b59c
pranavkhurana <pranavkhurana96@gmail.com>
2017-07-04 20:24:32 +0530

    added application 4

---
+++ src/main/java/entity/Author4.java
@@ -0,0 +1,59 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+public class Author4 {
+    @Id
+    String firstName;
+    String lastName;
+    int age;
+    @Temporal(TemporalType.DATE)
+    Date dateOfBirth;
+
+    public Author4() {
+    }
+
+    public Author4(String firstName, String lastName, int age, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
---
+++ src/main/java/mypack/Application4.java
@@ -0,0 +1,24 @@
+package mypack;
+
+import entity.Author4;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Application4 {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory=new Configuration().configure("hibernate4.cfg.xml").buildSessionFactory();
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Author4("John","Doe",21, new GregorianCalendar(1996,1,5).getTime()));
+        session.save(new Author4("Peter","Baelish",40, new GregorianCalendar(1977,5,24).getTime()));
+        session.save(new Author4("Davin","William",20, new GregorianCalendar(1997,6,12).getTime()));
+        session.save(new Author4("Jon","Snow",30, new GregorianCalendar(1987,11,10).getTime()));
+        session.getTransaction().commit();
+
+    }
+}
